6f67e52d8a398eaca6040f1636fe3a92efffbc3f
Benya De Leeuw <delben97@gmail.com>
2018-02-20 13:04:05 +0100

    svv_input -> svv_main

--- Reaction_Forces.py
+++ Reaction_Forces.py
@@ -1,10 +1,9 @@
 from math import *
 
 import scipy as np
-<<<<<<< HEAD
-from SSV_inputs import *
-=======
-from SVV_inputs import *
+
+#=======
+from SVV_main import *
 a = np.mat([1,2])
 b = np.mat([3,4])
 np.concatenate((a,b))
@@ -12,5 +11,5 @@ np.concatenate((a,b))
 # Eq 1:
 m1 = np.mat([1,1,1,0,0,sin(theta),0,0])
 
->>>>>>> 6b22186c115ae7137dad5d08bd1ab66ba85c7efc
+
 
