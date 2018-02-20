5ae7baefa0804767c252facb4c0de842aca94af7
NameIsFerran <ferran.v.kan@gmail.com>
2018-02-20 13:04:51 +0100

    Forces computation

--- Reaction_Forces.py
+++ Reaction_Forces.py
@@ -6,7 +6,22 @@ a = np.mat([1,2])
 b = np.mat([3,4])
 np.concatenate((a,b))
 
-# Eq 1:
-m1 = np.mat([1,1,1,0,0,sin(theta),0,0])
+# M*x=u
+# M is an 8x8 matrix with all the coefficients
+# x is the matrix with the unknowns ([F_H1_y, F_H2_y ,F_H3_y ,F_H1_z ,F_H2_z ,P_jam ,C ,L ])
+# u is the output matrix
 
+# Eq 1: Force equilibrium in y-direction
+m1 = np.mat([1,1,1,0,0,sin(theta),0,0])     
+u1 = np.mat([P*sin(theta)+q*l_a*sin(theta)])
 
+# Eq 2:
+m2 = np.mat([0,0,0,1,1,cos(theta),0,0])
+u2 = np.mat([P*cos(theta)-q*la*sin(theta)])
+
+# Eq 3: Moment equilibrium x-axis
+m3 = np.mat([0,0,0,0,0,(h_a/2.)*cos(theta)-(h_a/2.)*sin(theta),0,0])
+u3 = np.mat([P*(h_a/2)*cos(theta)-P*(h_a/2)*sin(theta)+q*l_a*cos(theta)*(0.25*C_a-(h_a/2.))])
+
+# Eq 4: Moment equilibrium y-axis
+m4 = np.mat([0,0,0,(x_2-x_1),0,(x_a/2)*cos(theta)
