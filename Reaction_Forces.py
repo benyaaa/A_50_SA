25e5054b5c53ad07ec48eac0ecbc670199ea9c92
NameIsFerran <ferran.v.kan@gmail.com>
2018-02-24 20:54:04 +0100

    kek

--- Reaction_Forces.py
+++ Reaction_Forces.py
@@ -10,9 +10,6 @@ from centroid_MOI import *
 
 I_zz = MOIZZAirfoil(t_st, h_st, w_st, BetaStringers, y_cst)
 
-
-
-
 # M*x=u
 # M is an 8x8 matrix with all the coefficients
 # x is the matrix with the unknowns ([F_H1_y, F_H2_y ,F_H3_y ,F_H1_z ,F_H2_z ,P_jam ,C ,L ])
@@ -27,14 +24,9 @@ m2 = np.mat([0.,0.,0.,1.,1.,cos(theta),0.,0.])
 u2 = np.mat([P*cos(theta)-q*l_a*sin(theta)])
 
 # Eq 3: Moment equilibrium x-axis
-
-
-m3 = np.mat([0.,0.,0.,0.,0.,(h/2.)*cos(theta)-(h/2.)*sin(theta),0.,0.])
-
+#m3 = np.mat([0.,0.,0.,0.,0.,(h/2.)*cos(theta)-(h/2.)*sin(theta),0.,0.])
 m3 = np.mat([0,0,0,0,0,(h/2.)*cos(theta)-(h/2.)*sin(theta),0,0])
-
-
-m3 = np.mat([0.,0.,0.,0.,0.,(h/2.)*cos(theta)-(h/2.)*sin(theta),0.,0.])
+#m3 = np.mat([0.,0.,0.,0.,0.,(h/2.)*cos(theta)-(h/2.)*sin(theta),0.,0.])
 u3 = np.mat([P*(h/2)*cos(theta)-P*(h/2)*sin(theta)+q*l_a*cos(theta)*(0.25*C_a-(h/2.))])
 
 # Eq 4: Moment equilibrium y-axis
@@ -83,4 +75,4 @@ L      = float(X[7])
 #print 'C      =', float(X[6])
 #print 'L      =', float(X[7])
 #print 'Deflection = ,', (L/(E*I_zz))/cos(theta) # in global cooridinate system
->>>>>>> 31c2b5204f0bee0dfdc92d5f186ce660bd372397
+
