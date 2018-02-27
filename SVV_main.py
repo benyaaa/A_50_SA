9d742db11343b0182c3de2ca3cd1b1a17e2340bf
Roemer <rmb.bakker@gmail.com>
2018-02-27 16:13:41 +0100

    hoi

--- SVV_main.py
+++ SVV_main.py
@@ -53,12 +53,11 @@ twist_4 = twister(l_a)
 delta_le_0 = delta_bend_0_y - eta*sin(twist_0)*cos(theta)
 delta_te_0 = delta_bend_0_y + (C_a-eta)*sin(twist_0)*cos(theta)
 
-<<<<<<< HEAD
+
 delta_le_4 = delta_bend_4 - eta*sin(twist_4)*cos(theta)
 delta_te_4 = delta_bend_4 + (C_a-eta)*sin(twist_4)*cos(theta)
 
 print M_booms
-=======
+
 delta_le_4 = delta_bend_4_y - eta*sin(twist_4)*cos(theta)
 delta_te_4 = delta_bend_4_y + (C_a-eta)*sin(twist_4)*cos(theta)
->>>>>>> 63169b95b6d2faf0b8a4e54064ae775ce7d8c14b
--- shearcentre idealised.py
+++ shearcentre idealised.py
@@ -11,12 +11,12 @@ from Idealised_structure2 import *
 from centroid_MOI import *
 
 
-S_y = 0 #imaginary force through shearcentre
-ksi = 0 #distance between trailing edge and shearcentre
+
 B_r_1_6 = IdealisedStructure(Q, y_cst, z_cst)[[0,1,2,3,4,5],20]
 B_y_1_6 = IdealisedStructure(Q, y_cst, z_cst)[[0,1,2,3,4,5],22]
 B_r_10_8 = IdealisedStructure(Q, y_cst, z_cst)[[9,8,7],20]
 B_y_10_8 = IdealisedStructure(Q, y_cst, z_cst)[[9,8,7],22]
+
 
 
 S_spse = (((pi*(h/2) / S_st) - 4) / 2) * S_st
@@ -32,7 +32,7 @@ CS_b_y_10_8 = np.append(CS_b_y_10_8, h/2)
 
 CS_b_z_10_8 = np.append(CentroidStringers(st_n, beta_sttr, C_a, h, S_st)[0][12], CentroidStringers(st_n, beta_sttr, C_a, h, S_st)[0][14])
 CS_b_z_10_8 = np.append(CS_b_z_10_8, C_a-h/2)
-q_b_1_6 = 0
+
 
 """def ShearCentreIdealised(B_r_1_6, B_r_10_8, B_y_1_6, B_y_10_8, MOIZZBoom, beta_st_10_8, CS_b_y_10_8, CS_b_z_10_8, q_b_1_6):
     q_b_1_6 = B_r_1_6*B_y_1_6/(MOIZZBoom(IdealisedStructure,CentroidAirfoil))
@@ -88,6 +88,7 @@ r_b_z_10_8 = CS_b_z_10_8
 M_b_x_10_8 = 2 * np.sum(F_b_y_10_8 * r_b_z_10_8 + F_b_z_10_8 * r_b_y_10_8)
 M_b_x_spar = (C_a-(h/2)) * h * q_b_7
 M_b_x = M_b_x_10_8 - M_b_x_spar
+
 
 
 
